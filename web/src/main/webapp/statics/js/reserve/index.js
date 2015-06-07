f98f5b3eb8cae76a7af2b19aaadd54ef043f9ded
Jiakun Xu <jiakunxu@gmail.com>
2015-06-07 12:22:22 +0800

    百度地图

--- web/src/main/webapp/statics/js/reserve/index.js
+++ web/src/main/webapp/statics/js/reserve/index.js
@@ -1,53 +1,14 @@
+// 百度地图API功能
+var map = new BMap.Map("allmap"); // 创建Map实例
+map.centerAndZoom('杭州', 15); // 初始化地图,设置中心点坐标和地图级别。
+map.addControl(new BMap.ZoomControl()); // 添加地图缩放控件
+var traffic = new BMap.TrafficLayer(); // 创建交通流量图层实例
+map.addTileLayer(traffic); // 将图层添加到地图上
+
 function goBack() {
 	loading();
 	top.location.href = appUrl + "/index.htm";
 }
-
-// 创建和初始化地图函数：
-function initMap() {
-	createMap();// 创建地图
-	setMapEvent();// 设置地图事件
-	addMapControl();// 向地图添加控件
-}
-
-// 创建地图函数：
-function createMap() {
-	var map = new BMap.Map("dituContent");// 在百度地图容器中创建一个地图
-	var point = new BMap.Point(120.169308, 30.281927);// 定义一个中心点坐标
-	map.centerAndZoom(point, 17);// 设定地图的中心点和坐标并将地图显示在地图容器中
-	window.map = map;// 将map变量存储在全局
-}
-
-// 地图事件设置函数：
-function setMapEvent() {
-	map.enableDragging();// 启用地图拖拽事件，默认启用(可不写)
-	map.enableScrollWheelZoom();// 启用地图滚轮放大缩小
-	map.enableDoubleClickZoom();// 启用鼠标双击放大，默认启用(可不写)
-	map.enableKeyboard();// 启用键盘上下左右键移动地图
-}
-
-// 地图控件添加函数：
-function addMapControl() {
-	// 向地图中添加缩放控件
-	var ctrl_nav = new BMap.NavigationControl({
-				anchor : BMAP_ANCHOR_TOP_LEFT,
-				type : BMAP_NAVIGATION_CONTROL_LARGE
-			});
-	map.addControl(ctrl_nav);
-	// 向地图中添加缩略图控件
-	var ctrl_ove = new BMap.OverviewMapControl({
-				anchor : BMAP_ANCHOR_BOTTOM_RIGHT,
-				isOpen : 1
-			});
-	map.addControl(ctrl_ove);
-	// 向地图中添加比例尺控件
-	var ctrl_sca = new BMap.ScaleControl({
-				anchor : BMAP_ANCHOR_BOTTOM_LEFT
-			});
-	map.addControl(ctrl_sca);
-}
-
-initMap();// 创建和初始化地图
 
 function loading() {
 	new Spinner({
