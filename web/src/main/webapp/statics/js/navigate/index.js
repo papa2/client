6ef8a7ead2151f7c920d5ea6d51d44310c468453
Jiakun Xu <jiakunxu@gmail.com>
2015-06-07 17:13:42 +0800

    百度地图

--- web/src/main/webapp/statics/js/navigate/index.js
+++ web/src/main/webapp/statics/js/navigate/index.js
@@ -1,9 +1,32 @@
 // 百度地图API功能
-var map = new BMap.Map("allmap"); // 创建Map实例
-map.centerAndZoom('杭州', 15); // 初始化地图,设置中心点坐标和地图级别。
-map.addControl(new BMap.ZoomControl()); // 添加地图缩放控件
-var traffic = new BMap.TrafficLayer(); // 创建交通流量图层实例
-map.addTileLayer(traffic); // 将图层添加到地图上
+function loadJScript() {
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "http://api.map.baidu.com/api?v=2.0&ak=57GsXSutGfXcANaX3GfW1FqZ&callback=init";
+	document.body.appendChild(script);
+}
+
+function init() {
+	var map = new BMap.Map("allmap"); // 创建Map实例
+	var point = new BMap.Point(120.175, 30.285);
+	map.centerAndZoom(point, 15);
+
+	var geolocation = new BMap.Geolocation();
+	geolocation.getCurrentPosition(function(r) {
+				if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+					var mk = new BMap.Marker(r.point);
+					map.addOverlay(mk);
+					map.panTo(r.point);
+					// alert('您的位置：' + r.point.lng + ',' + r.point.lat);
+				} else {
+					// alert('failed' + this.getStatus());
+				}
+			}, {
+				enableHighAccuracy : true
+			})
+}
+
+window.onload = loadJScript; // 异步加载地图
 
 function goBack() {
 	loading();
