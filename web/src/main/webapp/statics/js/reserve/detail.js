38b6b4373bb4347e13226029318d0c065b480925
Jiakun Xu <jiakunxu@gmail.com>
2015-09-02 16:30:30 +0800

    bug fix

--- web/src/main/webapp/statics/js/reserve/detail.js
+++ web/src/main/webapp/statics/js/reserve/detail.js
@@ -3,14 +3,21 @@ function back() {
 	top.location.href = appUrl + "/reserve/index.htm";
 }
 
-function QR() {
-	$('#QRCodeModal').modal('show');
+var qrcode = null;
 
-	var qrcode = new QRCode("qrcode", {
-				text : new Date().getTime(),
-				width : 300,
-				height : 300
-			});
+function QR() {
+	if (qrcode) {
+		qrcode.clear();
+		qrcode.makeCode(new Date().getTime().toString());
+	} else {
+		qrcode = new QRCode("qrcode", {
+					text : new Date().getTime().toString(),
+					width : 300,
+					height : 300
+				});
+	}
+
+	$('#QRCodeModal').modal('show');
 }
 
 function loading() {
