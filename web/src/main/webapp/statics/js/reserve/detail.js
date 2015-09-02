7ed37d7ed0dcd6531d0c03a9e6d98f7a95c024f8
Jiakun Xu <jiakunxu@gmail.com>
2015-09-02 16:13:22 +0800

    二维码

--- web/src/main/webapp/statics/js/reserve/detail.js
+++ web/src/main/webapp/statics/js/reserve/detail.js
@@ -3,6 +3,16 @@ function back() {
 	top.location.href = appUrl + "/reserve/index.htm";
 }
 
+function QR() {
+	$('#QRCodeModal').modal('show');
+
+	var qrcode = new QRCode("qrcode", {
+				text : new Date().getTime(),
+				width : 300,
+				height : 300
+			});
+}
+
 function loading() {
 	new Spinner({
 				top : '500%'
